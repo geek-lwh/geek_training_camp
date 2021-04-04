2c8651a347b18e95077755c9fa314e7cdf2a6f1c
monkey <monkey.d.luffer@gmail.com>
2021-04-04 22:21:38 +0800

    增加http request filter实现

--- src/week_03/hoursework_03/filter/HeaderHttpRequestFilter.java
+++ src/week_03/hoursework_03/filter/HeaderHttpRequestFilter.java
@@ -7,5 +7,6 @@ public class HeaderHttpRequestFilter implements HttpRequestFilter {
     @Override
     public void filter(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
         fullRequest.headers().set("name", "monkey");
+        fullRequest.headers().set("name2", "monkey2");
     }
 }
