b11f3a4abbc6497f6997066537ed6456527bd819
luweihong <303176776@qq.com>
2021-04-09 21:19:05 +0800

    move package

--- src/week_03/hoursework_03/HttpResponseFilter.java
+++
@@ -1,9 +0,0 @@
-package week_03.hoursework_03;
-
-import io.netty.handler.codec.http.FullHttpResponse;
-
-public interface HttpResponseFilter {
-
-    void filter(FullHttpResponse response);
-
-}
--- src/week_03/hoursework_04/HttpEndpointRouter.java
+++
@@ -1,8 +0,0 @@
-package week_03.hoursework_04;
-
-import java.util.List;
-
-public interface HttpEndpointRouter {
-
-    String select(List<String> endpoint);
-}
