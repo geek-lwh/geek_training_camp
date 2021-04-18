bcef54eb0655a3b2428bb164300fc2217fb41081
luweihong <303176776@qq.com>
2021-04-18 22:36:39 +0800

    作业2 xml + annotation形式注入

--- src/main/java/week_05/homework2/RunDemo.java
+++ src/main/java/week_05/homework2/RunDemo.java
@@ -2,6 +2,7 @@ package week_05.homework2;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import week_05.homework2.antotation.OneService;
 import week_05.homework2.xml.Student;
 
 public class RunDemo {
@@ -12,5 +13,11 @@ public class RunDemo {
         Student student = (Student) applicationContext.getBean("student");
         System.out.println("load spring bean from xml : " + student.toString());
         System.out.println("xml 形式注入结束");
+
+        System.out.println("annotation 形式注入开始");
+        OneService oneService = (OneService) applicationContext.getBean("oneService");
+        oneService.say();
+        System.out.println("annotation 形式注入结束");
+
     }
 }
