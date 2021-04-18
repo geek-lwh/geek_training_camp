f9b585e195f2687ddf0d742425bbc124b0c927c5
luweihong <303176776@qq.com>
2021-04-19 00:13:30 +0800

    单利模式总结

---
+++ src/main/java/week_05/homework5/HungrySingleton.java
@@ -0,0 +1,13 @@
+package week_05.homework5;
+
+public class HungrySingleton {
+
+    private static HungrySingleton instance = new HungrySingleton();
+
+    private HungrySingleton() {}
+
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
+
+}
